3e460e3ad9aba3b0282b2fbb3baf0510b0bf9ae0
Emma Söder Wikström <Emma_sw@hotmail.com>
2016-11-23 13:30:55 +0100

    Push it to the limnit

---
+++ workspace/goodShit/src/goodShit/goodShit.java
@@ -0,0 +1,14 @@
+package goodShit;
+
+import javax.swing.JOptionPane;
+
+public class goodShit {
+
+	public static void main(String[] args) {
+		int apa = JOptionPane.showConfirmDialog(null,  "du är en apa");
+		if (apa == apa){
+		JOptionPane.showMessageDialog(null, "Lögnare");
+
+		}
+	}
+}
