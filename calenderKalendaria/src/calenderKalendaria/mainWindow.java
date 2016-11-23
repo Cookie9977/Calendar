08704bbd3b47c51704adb69e30ae6410750e26d3
Emma Söder Wikström <Emma_sw@hotmail.com>
2016-11-23 12:55:36 +0100

    CalenderJava test

---
+++ calenderKalendaria/src/calenderKalendaria/mainWindow.java
@@ -0,0 +1,13 @@
+package calenderKalendaria;
+
+import javax.swing.JOptionPane;
+
+public class mainWindow {
+
+	public static void main(String[] args) {
+		String str = "Kolla på mig";
+		JOptionPane.showMessageDialog(null,str);
+
+	}
+
+}
