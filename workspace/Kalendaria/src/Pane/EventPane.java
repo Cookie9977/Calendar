bb20b104e7e0ef980e8902fe0490c6aea0add44c
Dennis Johansson <t971016dejo@gmail.com>
2016-11-25 14:59:07 +0100

    eventpane1.1

--- workspace/Kalendaria/src/Pane/EventPane.java
+++ workspace/Kalendaria/src/Pane/EventPane.java
@@ -1,8 +1,10 @@
-package window;
+package Pane;
 
+import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 
+import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
@@ -18,20 +20,35 @@ public class EventPane extends JPanel{
 	public EventPane() {
 		//declaring
 		labels = new JLabel[6];
-		textFields = new JTextField[4];
+		textFields = new JTextField[2];
 		description = new JTextArea();
-		
+		//set labels
 		for (int i = 0; i < labelText.length; i++) {
 			labels[i] = new JLabel(labelText[i]);
 		}
-		
-		//set labels
-		//labels[5].setLabelFor(description);
-		add(description);
+		for (int i = 0; i < textFields.length; i++) {
+			textFields[i] = new JTextField();
+			textFields[i].setPreferredSize(new Dimension(175, 20));
+			labels[i].setLabelFor(textFields[i]);
+			add(labels[i]);
+			add(textFields[i]);
+			
+		}
 		//layout
 		GridBagLayout gridBag = new GridBagLayout();
 		GridBagConstraints gbc = new GridBagConstraints();
 		setLayout(gridBag);
+		
+		//description setup
+		description.setPreferredSize(new Dimension(175, 100));
+		description.setBorder(BorderFactory.createEtchedBorder());
+		labels[5].setLabelFor(description);
+		
+		
+		add(labels[5]);
+		add(description);
+	
+
 	}
 
 }
--- workspace/Kalendaria/src/main/Main.java
+++ workspace/Kalendaria/src/main/Main.java
@@ -2,18 +2,16 @@ package main;
 
 import javax.swing.JOptionPane;
 
-import window.EventPane;
+import Pane.EventPane;
 import window.Window;
 
 public class Main {
 	public final static JavaDB db = new JavaDB("192.168.216.168", "Test", "qwerty123", "calendar");
 	public static void main(String[] args) {
-		new Window();
-		//EventPane pane = new EventPane();
-		//int test = JOptionPane.showConfirmDialog(null, pane, "test", JOptionPane.OK_CANCEL_OPTION);
-		//if (test == JOptionPane.OK_OPTION) {
-			
-		//}
+		//new Window();
+		EventPane pane = new EventPane();
+		int test = JOptionPane.showConfirmDialog(null, pane, "test", JOptionPane.OK_CANCEL_OPTION);
+		
 	}
 
 }
