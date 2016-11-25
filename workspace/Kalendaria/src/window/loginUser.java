e97f64125a68ea1bd7e89ae493100f388dafba6e
Emma Söder Wikström <Emma_sw@hotmail.com>
2016-11-25 12:38:53 +0100

    asdasd

--- workspace/Kalendaria/src/window/loginUser.java
+++ workspace/Kalendaria/src/window/loginUser.java
@@ -5,27 +5,33 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import javax.swing.JButton;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
-public class registerUser extends JPanel implements ActionListener{
+
+import main.JavaDB;
+
+public class loginUser extends JPanel implements ActionListener{
 private JTextField[] textBox;
+protected final JavaDB DB = new JavaDB("192.168.216.168","root","","calendar");
 
 	private static final long serialVersionUID = -2354560402587167649L;
 
-	public registerUser(){
+	public loginUser(){
 		setBackground(new Color(123,123,123));
 		setSize(new Dimension(100,100));
 		setVisible(true);
 		Font inputFont = new Font("SansSerif",Font.BOLD,20);
 
 		String typeInput[];
-		typeInput = new String[] { "Användarnamn", "Email","Lösenord"};
+		typeInput = new String[] { "Email","Lösenord"};
 		
-		textBox = new JTextField[2];
+		textBox = new JTextField[1];
 		JPasswordField password = new JPasswordField();
 		for (int i = 0; i < textBox.length; i++) {
 				
@@ -40,18 +46,25 @@ private JTextField[] textBox;
 		add(password);
 		
 		JButton submitButton = new JButton();
-		submitButton.setText("Fuckoff");
+		submitButton.setText("Logga in");
 		submitButton.addActionListener(this);
 		add(submitButton);
 		
 	}
+	String email = textBox[0].getText();
+	String password = textBox[1].getText();
 	
-	public void actionPerformed(ActionEvent e){
-		String SQL="INSERT INTO (username,email,password) values('"+textBox[0].getText()+"','"+textBox[1].getText()+"','"+textBox[2].getText()+"');";
-		
+	public void displayUsers(String email, String password){
+		String SQL="SELECT email,password FROM user VALUES('?','?');";
+		DB.getData(SQL);
+}
+
+	public void actionPerformed(ActionEvent e){			
+				displayUsers(email,password);
+			}
 //		System.out.print(textBox[0].getText());
 //		System.out.print(textBox[1].getText());
 //		System.out.print(textBox[2].getText());
-		//db.execute(SQL);
-	}
+		
+	
 }
