5a7f208212306553578a8fa6fbaca4545ba13dca
Pontus Erlandsson <wnedpro@live.se>
2016-11-30 10:12:22 +0100

    Ny nav bar och formatting

--- workspace/Kalendaria/src/window/MenuNavBar.java
+++ workspace/Kalendaria/src/window/MenuNavBar.java
@@ -1,5 +1,48 @@
 package window;
 
-public class MenuNavBar {
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class MenuNavBar extends JPanel implements ActionListener {
+	private JFrame frame;
+	private int type;
+	private String[] buttonText = { "Månad", "Vecka", "Dag" };
+
+	private static final long serialVersionUID = -6222480473508548550L;
+
+	public MenuNavBar(JFrame frame, int type) {
+		this.frame = frame;
+		this.type = type;
+		JButton button = new JButton();
+
+		switch (type) {
+		case 0: // Månad
+			button.setText(buttonText[0]);
+			break;
+		case 1: // Vecka
+			button.setText(buttonText[1]);
+			break;
+		case 2: // Dag
+			button.setText(buttonText[2]);
+			break;
+		}
+		button.addActionListener(this);
+		add(button);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent AE) {
+		String buttonName = ((JButton) AE.getSource()).getText();
+		JOptionPane.showMessageDialog(null, "du vill hit: " + buttonName + " !");
+
+	}
 
 }
