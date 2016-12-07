83ac25fdd038da3dddeda9dbeac4e7f520b50feb
Emma Söder Wikström <Emma_sw@hotmail.com>
2016-12-07 14:54:51 +0100

    MonthView Samt Tabell help me with Clicklistener plox

--- workspace/Kalendaria_final/src/window/MonthView.java
+++ workspace/Kalendaria_final/src/window/MonthView.java
@@ -4,6 +4,10 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.ArrayList;
 
 import javax.swing.BorderFactory;
@@ -12,14 +16,17 @@ import javax.swing.JPanel;
 import javax.swing.JTable;
 
 import logic.TimeLogic;
+import main.ClickListener;
 
-public class MonthView extends JPanel {
+public class MonthView extends JPanel implements MouseListener {
 
 	private static final long serialVersionUID = 6304391601622162482L;
 	private JTable tableMonth;
 	private pane.TimePane TimePane;
 	private TimeLogic TimeLogic;
-	
+	public 	int FmonthColumn;
+	public int LmonthRow;
+	public int LmonthColumn;
 
 	public MonthView() {
 		TimePane = new pane.TimePane();
@@ -84,63 +91,72 @@ public class MonthView extends JPanel {
 		
 		
 		
-		tableMonth = new JTable(6, 7);
+		tableMonth = new JTable(6, 7){
+			public boolean isCellEditable(int aRow, int aColumn) {
+
+				 return false;
+				}
+		};
 		tableMonth.setTableHeader(null);
 		tableMonth.setRowHeight(92);
-		tableMonth.getColumnModel().getColumn(0).setCellRenderer(new TabellRenderare());
-		tableMonth.setCellSelectionEnabled(true);
-		tableMonth.setEnabled(false);
-		int monthColumn;
+		tableMonth.getColumnModel().getColumn(0).setCellRenderer(new TabellRenderare(this));
+		tableMonth.setCellSelectionEnabled(true);	
+		tableMonth.addMouseListener(new ClickListener(this){
+			@Override
+			public void DoubleClick(MouseEvent e) {
+				JTable source = (JTable)e.getSource();
+		        int row = source.rowAtPoint( e.getPoint() );
+		        int column = source.columnAtPoint( e.getPoint() );
+		        
+		        if(source.isRowSelected(row)){
+		        	System.out.println("Row:" +row+ "Happy birthday!");
+		        	
+		        }
+		        if (! source.isRowSelected(row))
+		            source.changeSelection(row, column, false, false);
+				System.out.println("Col: "+column+ " Row: "+row);
+			}
 
+			
+			
+		});
 		containDays.add(tableMonth);
 		
 		ArrayList<String> dag = TimeLogic.getWeekday();
 		System.out.println(dag);
-		int thisMonth = TimeLogic.getCurrentMonth();
-		int firstDayOfMonth = TimeLogic.firstDayMonth();
 		int lastDayOfMonth = TimeLogic.lastDayMonth();
 		int dayOfWeek = TimeLogic.dayOfWeek();
 		int firstDayNextMonth = TimeLogic.firstDayNextMonth();
 		int lastDayLastMonth = TimeLogic.lastDayLastMonth();
 		int justering = dayOfWeek;
 		//int justering = TimeLogic.justering();
-		ArrayList<String> daysOfMonth = TimeLogic.getDays(""+thisMonth);
 		int lopNummer = 1;
-		
-		
-		
 		
 		for (int j = 0; j < tableMonth.getRowCount(); j++) {
 			for (int i = 0; i < tableMonth.getColumnCount(); i++) {
-			tableMonth.getColumnModel().getColumn(j+1).setCellRenderer(new TabellRenderare());
-				
+			tableMonth.getColumnModel().getColumn(j+1).setCellRenderer(new TabellRenderare(this));
+			
 				if(lopNummer > 1 && lopNummer<lastDayOfMonth+1 ){
 					tableMonth.setValueAt(lopNummer, j, i);
-					
-					//tableMonth.getRowCount().getRow(i).setCellRenderer(new TabellRenderare());
 					lopNummer++;
 				
 				}else if(j==0 && dayOfWeek == i){
 					tableMonth.setValueAt(lopNummer, j, i);
-//					tableMonth.getColumnModel().getColumn(j).setCellRenderer(new TabellRenderare());
-//					System.out.println(i);
-//					System.out.println(j);
 					lopNummer++;	
 					
 				}else if(lopNummer>lastDayOfMonth){
 					tableMonth.setValueAt(firstDayNextMonth++, j, i);	
-//					tableMonth.getColumnModel().getColumn(j).setCellRenderer(new TabellRenderare());
+					LmonthColumn = i;
+					LmonthRow = j;
+					System.out.println("Column "+LmonthColumn+ " Row "+ LmonthRow);
 				}
 				
 				else if(lopNummer<lastDayLastMonth){
 					tableMonth.setValueAt(lastDayLastMonth-justering+1, j, (dayOfWeek-justering));
-//					tableMonth.getColumnModel().getColumn(j).setCellRenderer(new TabellRenderare());
 					justering--;
-					monthColumn = i;
-					System.out.println("MonthCol = "+monthColumn);
+					FmonthColumn = i;
 				}	
 			}
 		}
 	}
-
 }
--- workspace/Kalendaria_final/src/window/TabellRenderare.java
+++ workspace/Kalendaria_final/src/window/TabellRenderare.java
@@ -2,22 +2,34 @@ package window;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Font;
+import java.time.Month;
 import java.util.ArrayList;
 
+import javax.swing.JLabel;
 import javax.swing.JTable;
+import javax.swing.SwingConstants;
 import javax.swing.table.DefaultTableCellRenderer;
 
 import logic.TimeLogic;
 
 public class TabellRenderare extends DefaultTableCellRenderer {
 	private TimeLogic TimeLogic;
-	private MonthView monthRow;
-	private MonthView monthColumn;
+	private MonthView MonthViewColumnRow;
 	
-	 @Override public Component getTableCellRendererComponent(
+	 public TabellRenderare(MonthView monthView) {
+		 super();
+		 this.MonthViewColumnRow = monthView;
+		 
+		// TODO Auto-generated constructor stub
+	}
+	 
+
+	
+	@Override public Component getTableCellRendererComponent(
 			    JTable aTable, Object aNumberValue, boolean aIsSelected, 
 			    boolean aHasFocus, int aRow, int aColumn
 			  ) {  
+		
 			    /* 
 			    * Implementation Note :
 			    * It is important that no 'new' objects be present in this 
@@ -25,26 +37,47 @@ public class TabellRenderare extends DefaultTableCellRenderer {
 			    * if the table is large, then a large number of objects would be 
 			    * created during rendering.
 			    */
+				JLabel renderedLabel = (JLabel) super.getTableCellRendererComponent(aTable, aNumberValue, aIsSelected, aHasFocus, aRow, aColumn);
+				
 		 		
-		 	//	MonthView monthColumn = MonthView.MonthView().monthColumn;
+		 		//System.out.println("Raden här"+MonthViewColumnRow.FmonthRow);
 			    if (aNumberValue == null) return this;
 			    Component renderer = super.getTableCellRendererComponent(
 			      aTable, aNumberValue, aIsSelected, aHasFocus, aRow, aColumn
 			    );
+			    
 			    Number value = (Number)aNumberValue;
-			    if (aColumn == 6) {
-			    	renderer.setFont(new Font("Sansserif", Font.BOLD, 15));
+			    
+			    
+			    renderer.setBackground(new Color(238, 238, 238));
+			    renderer.setForeground(new Color(55, 55, 55));
+			    renderedLabel.setHorizontalAlignment(LEFT);
+		    	renderedLabel.setVerticalAlignment(TOP);
+		    	renderer.setFont(new Font("SansSerif", Font.PLAIN, 15));
+		    	
+		    	if(aColumn <= MonthViewColumnRow.FmonthColumn && aRow == 0) {
+				      renderer.setBackground(new Color(178, 178, 178));
+				      renderer.setForeground(new Color(238,238,238));
+				      renderer.setFont(new Font("SansSerif", Font.ITALIC, 15));
+				      
+				    }
+		    	
+		    	else if(aRow >= MonthViewColumnRow.LmonthRow && aColumn <= MonthViewColumnRow.LmonthColumn) {
+		    		 renderer.setBackground(new Color(178, 178, 178));
+				      renderer.setForeground(new Color(238,238,238));
+				      renderer.setFont(new Font("SansSerif", Font.ITALIC, 15));
+				    //aColumn <= MonthViewColumnRow.LmonthColumn && aRow == MonthViewColumnRow.LmonthRow
+					    System.out.println("Column "+ aColumn);
+					    System.out.println("Row "+aRow);
+				    }
+		    	else if (aColumn == 6) {	
 			    	renderer.setForeground(Color.RED);
-//			      renderer.setBackground(Color.red);
-			    }
-			    else if(aColumn < 7){
-			    	renderer.setFont(new Font("Sansserif", Font.BOLD, 15));
-			    }
-//			    else if(aColumn == monthCol) {
-//			      renderer.setBackground(Color.BLUE);
-//			    }
+			    
+		    	}
 			    return this;
+			    
 			  }
-			  
+			
+		
 
 }
